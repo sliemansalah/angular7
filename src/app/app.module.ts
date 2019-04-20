f8a14165da1bb8291e090734c6240a55b0107d4a
Slieman <slieman2001@hotmail.com>
2019-04-21 00:32:57 +0300

    #12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning.component';
  
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
   imports: [
     BrowserModule,
