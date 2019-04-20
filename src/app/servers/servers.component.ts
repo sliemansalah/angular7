996ec5946a6c9ba00e5c621ef88ff2b3111bade5
Slieman <slieman2001@hotmail.com>
2019-04-20 23:31:16 +0300

    #10

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: '<app-server></app-server><h3>Hi man</h3><p>Thank you.</p>',
-  styles: ['p{color:red;font-size:16px;}']
+  styles: [
+    'p{color:red;font-size:16px;}'
+  ]
 })
 export class ServersComponent implements OnInit {
 
