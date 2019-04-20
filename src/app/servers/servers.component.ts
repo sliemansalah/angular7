0496589dba1cbf3dd7cad33fd2b733adaafa2c5a
Slieman <slieman2001@hotmail.com>
2019-04-20 23:38:54 +0300

    #11

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: '<app-server></app-server><h3>Hi man</h3><p>Thank you.</p>',
+  selector: '.app-servers',
+  template: '<h3>Hi man</h3><p>Thank you.</p>',
   styles: [
     'p{color:red;font-size:16px;}'
   ]
