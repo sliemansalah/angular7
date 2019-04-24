de24d4996ae33c02578261653a0b28dcae5127b7
Slieman <slieman2001@hotmail.com>
2019-04-25 01:38:18 +0300

    #31

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -13,6 +13,6 @@ export class ServerComponent {
     ngOnInit() { }
     onToggleDetails(){
         this.showSecret=!this.showSecret;
-        this.log.push(this.log.length+1);
+        this.log.push(new Date());
     }
 }
