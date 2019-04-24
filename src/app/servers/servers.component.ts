b611e357285f73f7e46393e9bfc5161689adff3c
Slieman <slieman2001@hotmail.com>
2019-04-25 01:11:32 +0300

    #29

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer=false;
   serverCreated=false;
   serverCreationStatus='No server was created!';
+  servers=['TestServer','newServer']
   constructor() { 
     setInterval(() => {
       this.serverCreated=false;
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer(){
     this.serverCreated=true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus='Server was created! Name is' + this.serverName;
   }
   onUpdateServerName(event:Event){
