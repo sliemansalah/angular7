a637e54fa5efe2883a34a96e9074c62059cccad8
Slieman <slieman2001@hotmail.com>
2019-04-28 23:27:50 +0300

    #34

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning.component';
 import { FormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
+    AppComponent, 
   ],
   imports: [
     BrowserModule,
