f3926a9132c642e5d7648e2c780137a24bbcdd9b
Slieman <slieman2001@hotmail.com>
2019-05-16 03:03:39 +0300

    #49

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
-  @Output() bluePrintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   newServerName = '';
   newServerContent = '';
   constructor() { }
