fc8bffb5ac3304190356a7c3e0f3dd6b6063235f
Slieman <slieman2001@hotmail.com>
2019-05-16 03:25:26 +0300

    updated

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server', name:'Testserver', content:'Just a test!'}];
-
+ 
   onServerAdded(serverData:{serverName:string, serverContent:string}) {
     this.serverElements.push({
       type: 'server',
